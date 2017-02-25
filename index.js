70157036762e9bde36d46c27760ea87dd826bb22
Heorhi Marazenka <yoga.yogich@gmail.com>
2017-02-25 22:33:50 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
 var teams = 
-    	[{userId: "1756677", name: "xheo", result: 1209},
-	{userId: "1756596", name: "pr-positive", result: 1188},
-	{userId: "1756645", name: "cron314", result: 1042},	
-	{userId: "1756613", name: "busotir", result: 1136},
-	{userId: "1765524", name: "PrincipessaMilana", result: 1159},
-	{userId: "1756537", name: "ymat", result: 1152}]; 
+    	[{userId: "1756677", name: "xheo", result: 1262},
+	{userId: "1756596", name: "pr-positive", result: 1261},
+	{userId: "1756645", name: "cron314", result: 1076},	
+	{userId: "1756613", name: "busotir", result: 1203},
+	{userId: "1765524", name: "PrincipessaMilana", result: 1220},
+	{userId: "1756537", name: "ymat", result: 1215}]; 
 var ids = 0;
 //var SportsRuUrlTemplate = "https://www.sports.ru/fantasy/football/team/points/";
 var SportsRuUrlTemplate = "https://crossorigin.me/http://www.sports.ru/fantasy/football/team/points/";
-var JsonUrlTemplate = "/7592.json";
+var JsonUrlTemplate = "/7593.json";
 
 a();
 
