c85fce8c52c1b0399c480fd13b7229d189e67e75
Heorhi Marazenka <yoga.yogich@gmail.com>
2017-09-12 17:41:06 +0300

    Update ucl.js

--- ucl.js
+++ ucl.js
@@ -1,7 +1,7 @@
 var teams = 
     	[{userId: "1910925", name: "xheo", result: 0}, 
-	{userId: "1908919", name: "pr-positive", result: 0},	
-	{userId: "1910480", name: "busotir", result: 0}];
+	{userId: "1910480", name: "pr-positive", result: 0},	
+	{userId: "1908919", name: "busotir", result: 0}];
 var ids = 0;
 //var SportsRuUrlTemplate = "https://www.sports.ru/fantasy/football/team/points/";
 //var SportsRuUrlTemplate = "https://crossorigin.me/http://www.sports.ru/fantasy/football/team/points/";
