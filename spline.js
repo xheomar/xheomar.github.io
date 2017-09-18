6e30aeedf8999e80dc7bc2f42463b9726f4db041
Heorhi Marazenka <yoga.yogich@gmail.com>
2017-09-18 23:17:19 +0300

    Update spline.js

--- spline.js
+++ spline.js
@@ -50,7 +50,8 @@ var data = [
 	{tour: 0, xheo: 0, positive: 0, busotir: 0, Евгений: 0, PrincipessaMilana: 0, ymat: 0},
 	{tour: 1, xheo: 54, positive: 52, busotir: 0, Евгений: 60, PrincipessaMilana: 64, ymat: 61},
 	{tour: 2, xheo: 115, positive: 86, busotir: 68, Евгений: 103, PrincipessaMilana: 133, ymat: 128},
-	{tour: 3, xheo: 158, positive: 151, busotir: 120, Евгений: 134, PrincipessaMilana: 180, ymat: 173}
+	{tour: 3, xheo: 158, positive: 151, busotir: 120, Евгений: 134, PrincipessaMilana: 180, ymat: 173},
+	{tour: 4, xheo: 214, positive: 204, busotir: 212, Евгений: 178, PrincipessaMilana: 266, ymat: 246}
 ];	
 
   new FancyGrid({
