530a61a5f9f1908d504c90598ce0458297f7d374
Heorhi Marazenka <yoga.yogich@gmail.com>
2017-09-20 11:30:13 +0300

    Update parser.js

--- parser.js
+++ parser.js
@@ -1,23 +1,24 @@
 $('form').submit(function() {
 event.preventDefault();
-var SportsRuUrlTemplate = "https://www.sports.ru/api/comment/get/message.json?order_type=old&limit=100&message_class=Sports%3A%3ABlog%3A%3APost%3A%3APost&new_time=1&style=newjs&message_id=";
+var SportsRuUrlTemplate = "https://cors.io/?https://www.sports.ru/api/comment/get/message.json?order_type=old&limit=100&message_class=Sports%3A%3ABlog%3A%3APost%3A%3APost&new_time=1&style=newjs&message_id=";
 if ($('.input').val() !== '') {
 
     var newTask = $('.input').val();
     var JsonUrlTemplate = newTask.match(/\d{7}/)[0];
-    var yql_url = 'https://query.yahooapis.com/v1/public/yql';
+    //var yql_url = 'https://query.yahooapis.com/v1/public/yql';
     var json_url = SportsRuUrlTemplate + JsonUrlTemplate;
     $.ajax({
-      'url': yql_url,
-      'data': {
-        'q': 'SELECT * FROM json WHERE url="'+json_url+'"',
-        'format': 'json',
-        'jsonCompat': 'new',
-      },
-      'dataType': 'jsonp',
+      'url': json_url,
+      //'data': {
+      //  'q': 'SELECT * FROM json WHERE url="'+json_url+'"',
+      //  'format': 'json',
+      //  'jsonCompat': 'new',
+      //},
+      'dataType': 'json',
       'success': function(response) {
 
-       var comments = response.query.results.json.data.comments;
+       //var comments = response.query.results.json.data.comments;
+       var comments = response.data.comments;
        for (var i = 0; i < comments.length; i++)
        {
           var texts = "";
