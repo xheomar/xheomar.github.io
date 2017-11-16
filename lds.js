33f714658d2ce8054afb1abf8db24a9b7aefccf6
xheomar <yoga.yogich@gmail.com>
2017-11-17 01:58:17 +0300

    17-11-2017

---
+++ lds.js
@@ -0,0 +1,11 @@
+var data_lds = [
+ { name: 'Australia. A-League', date: '2017/11/17', event: 'AnyOther', count: '1', games: '8.70 (11:50)  '}
+, { name: 'Austria. Regionalliga', date: '2017/11/17', event: 'AnyOther', count: '4', games: '4.25 (21:00) 8.20 (21:00) 3.72 (21:30) 7.40 (21:30)  '}
+, { name: 'England. Premier League 2', date: '2017/11/17', event: 'AnyOther', count: '1', games: '4.50 (22:00)  '}
+, { name: 'Germany. Regionalliga North', date: '2017/11/17', event: 'AnyOther', count: '1', games: '7.50 (21:00)  '}
+, { name: 'Germany. Regionalliga Northeast', date: '2017/11/17', event: 'AnyOther', count: '1', games: '7.80 (21:00)  '}
+, { name: 'Germany. Regionalliga Southwest', date: '2017/11/17', event: 'AnyOther', count: '2', games: '7.60 (21:00) 8.30 (21:00)  '}
+, { name: 'Germany. Regionalliga West', date: '2017/11/17', event: 'AnyOther', count: '2', games: '7.10 (21:30) 8.60 (21:30)  '}
+, { name: 'Netherlands. Eerste Divisie', date: '2017/11/17', event: 'AnyOther', count: '8', games: '4.75 (22:00) 5.20 (22:00) 5.30 (22:00) 5.90 (22:00) 6.50 (22:00) 7.10 (22:00) 7.20 (22:00) 8.90 (22:00)  '}
+, { name: 'UAE. Arabian Gulf League', date: '2017/11/17', event: 'Over_4.5', count: '3', games: '4.70 (15:40) 4.55 (15:45) 3.78 (18:45)  '}
+];
