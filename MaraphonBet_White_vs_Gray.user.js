db3ef2f01c31412fa6d02225f44e9ddbd65f5d33
xheomar <yoga.yogich@gmail.com>
2017-12-09 15:38:49 +0300

    ddddd

--- MaraphonBet_White_vs_Gray.user.js
+++ MaraphonBet_White_vs_Gray.user.js
@@ -1,10 +1,10 @@
 // ==UserScript==
 // @name         MaraphonBet_White_vs_Gray
 // @namespace    http://tampermonkey.net/
-// @version      0.1
+// @version      0.2
 // @description  find AnyOtherScore on the page and click on it!
 // @author       xheomar
-// @match        https://www.marathonbet.by/su/*
+// @match        https://www.marathonbet.ru/su/*
 // @grant        none
 // ==/UserScript==
 
@@ -25,8 +25,7 @@
     }
     else
     {
-        var isWhite = confirm("OK - белые линии, Cancel - серые");
-        // alert( isWhite );
+        var isWhite = confirm("OK - белые линии, Cancel - перейти к выбору серых");
         if ( isWhite )
         {
             // Выбираем белые строчки со ставками на ЛДС
@@ -46,15 +45,19 @@
         }
         else
         {
-            // Выбираем серые строчки со ставками на ЛДС
-            setTimeout(function(){
-                $(".bg .event-more-view").click();
+            var isGray = confirm("OK - серые линии, Cancel - отмена");
+            if ( isGray )
+            {
+                // Выбираем серые строчки со ставками на ЛДС
                 setTimeout(function(){
-                    $("tr td:contains('Любой другой счёт')").filter(".height-column-with-price").click();
+                    $(".bg .event-more-view").click();
                     setTimeout(function(){
-                        $(".bg .event-more-view").click();}, 3000);
+                        $("tr td:contains('Любой другой счёт')").filter(".height-column-with-price").click();
+                        setTimeout(function(){
+                            $(".bg .event-more-view").click();}, 3000);
+                    }, 1500);
                 }, 1500);
-            }, 1500);
+            }
         }
     }
 })();
