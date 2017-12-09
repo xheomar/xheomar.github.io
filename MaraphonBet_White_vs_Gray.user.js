d435f528a477860e7875ccb825cc92ba201c4d74
xheomar <yoga.yogich@gmail.com>
2017-12-09 15:18:57 +0300

    UserScript.js

---
+++ MaraphonBet_White_vs_Gray.user.js
@@ -0,0 +1,60 @@
+// ==UserScript==
+// @name         MaraphonBet_White_vs_Gray
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  find AnyOtherScore on the page and click on it!
+// @author       xheomar
+// @match        https://www.marathonbet.by/su/*
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+    var isSelectAll = confirm("OK - выбрать все ЛДС на странице, Cancel - перейти к выбору чёт/нечет");
+    if ( isSelectAll )
+    {
+        // Выбираем все строчки со ставками на ЛДС
+        setTimeout(function(){
+            $(".event-more-view").click();
+                setTimeout(function(){
+                    $("tr td:contains('Любой другой счёт')").filter(".height-column-with-price").click();
+                    setTimeout(function(){
+                        $(".event-more-view").click();}, 3000);
+                }, 1500);
+        }, 1500);
+    }
+    else
+    {
+        var isWhite = confirm("OK - белые линии, Cancel - серые");
+        // alert( isWhite );
+        if ( isWhite )
+        {
+            // Выбираем белые строчки со ставками на ЛДС
+            setTimeout(function(){
+                $(".event-more-view").click();
+                setTimeout(function(){
+                    $(".bg .event-more-view").click();
+                    setTimeout(function(){
+                        $("tr td:contains('Любой другой счёт')").filter(".height-column-with-price").click();
+                        setTimeout(function(){
+                            $(".bg .event-more-view").click();
+                            setTimeout(function(){$(".event-more-view").click();}, 2000);
+                        }, 3000);
+                    }, 1500);
+                }, 1500);
+            }, 1500);
+        }
+        else
+        {
+            // Выбираем серые строчки со ставками на ЛДС
+            setTimeout(function(){
+                $(".bg .event-more-view").click();
+                setTimeout(function(){
+                    $("tr td:contains('Любой другой счёт')").filter(".height-column-with-price").click();
+                    setTimeout(function(){
+                        $(".bg .event-more-view").click();}, 3000);
+                }, 1500);
+            }, 1500);
+        }
+    }
+})();
