a05134ca9618d7c443924986d817d10325a18506
xheomar <yoga.yogich@gmail.com>
2018-01-22 02:36:07 +0300

    465465

--- lds.js
+++ lds.js
@@ -1,21 +1,28 @@
 var data_lds = [
- { name: 'MARATHONBET онлайн букмекер', date: '2018/01/17', event: 'AnyOther', count: '2', avg: '5.80', lds: '-', diff: '-', games: '14.50 20.00 '}
-, { name: 'Чемпионат африканских наций. 2018. Финальный турнир. Марокко. Групповой этап', date: '2018/01/17', event: 'AnyOther', count: '2', avg: '7.66', lds: '-', diff: '-', games: '6.10 20.00 '}
-, { name: 'Англия. Кубок. 3-й раунд. Переигровка', date: '2018/01/17', event: 'AnyOther', count: '3', avg: '9.68', lds: '-', diff: '-', games: '17.50 4.00 9.50 '}
-, { name: 'Англия. Трофей Футбольной лиги. 1/8 финала', date: '2018/01/17', event: 'AnyOther', count: '1', avg: '13.51', lds: '-', diff: '-', games: '7.40 '}
-, { name: 'Бразилия. Лига Кариока. Серия A. Гуанабара', date: '2018/01/17', event: 'AnyOther', count: '4', avg: '9.50', lds: '-', diff: '-', games: '5.10 10.50 13.00 13.50 '}
-, { name: 'Бразилия. Лига Паулиста. Серия A1', date: '2018/01/17', event: 'AnyOther', count: '1', avg: '7.69', lds: '-', diff: '-', games: '13.00 '}
-, { name: 'Бразилия. Северо-восточный кубок. Групповой этап', date: '2018/01/17', event: 'AnyOther', count: '3', avg: '8.31', lds: '-', diff: '-', games: '17.00 9.90 9.20 '}
-, { name: 'Египет. Премьер-лига', date: '2018/01/17', event: 'AnyOther', count: '3', avg: '6.52', lds: '7.45', diff: '0.93', games: '14.50 15.00 16.50 '}
-, { name: 'Индия. Суперлига', date: '2018/01/17', event: 'AnyOther', count: '1', avg: '5.71', lds: '14.58', diff: '8.87', games: '17.50 '}
-, { name: 'Индонезия. Кубок президента. Групповой этап. Тенггаронг', date: '2018/01/17', event: 'AnyOther', count: '2', avg: '18.59', lds: '-', diff: '-', games: '3.56 7.20 '}
-, { name: 'Испания. Кубок. 1/4 финала. Первые матчи', date: '2018/01/17', event: 'AnyOther', count: '3', avg: '20.31', lds: '-', diff: '-', games: '4.55 6.40 3.82 '}
-, { name: 'Кипр. 1-й дивизион', date: '2018/01/17', event: 'AnyOther', count: '2', avg: '14.39', lds: '17.52', diff: '3.13', games: '8.00 5.90 '}
-, { name: 'Кипр. Кубок', date: '2018/01/17', event: 'AnyOther', count: '2', avg: '12.63', lds: '25.00', diff: '12.37', games: '12.50 3.34 '}
-, { name: 'Мексика. Кубок. Групповой этап', date: '2018/01/17', event: 'AnyOther', count: '6', avg: '10.22', lds: '-', diff: '-', games: '10.00 10.50 8.60 15.50 8.70 5.40 '}
-, { name: 'ОАЭ. Кубок Президента. 1/8 финала', date: '2018/01/17', event: 'AnyOther', count: '3', avg: '22.19', lds: '-', diff: '-', games: '3.22 4.50 5.80 '}
-, { name: 'Турция. Кубок. 1/8 финала. Ответный матч', date: '2018/01/17', event: 'AnyOther', count: '3', avg: '13.64', lds: '-', diff: '-', games: '9.70 5.90 6.40 '}
-, { name: 'Франция. Лига 1', date: '2018/01/17', event: 'AnyOther', count: '7', avg: '7.46', lds: '9.36', diff: '1.90', games: '11.50 12.50 15.00 21.00 23.00 9.10 1.69 '}
-, { name: 'ЮАР. Премьер-лига', date: '2018/01/17', event: 'AnyOther', count: '1', avg: '5.41', lds: '3.79', diff: '-1.62', games: '18.50 '}
-, { name: 'Женщины. Чемпионат Южной Америки до 20 лет. 2018. Эквадор. Групповой этап', date: '2018/01/17', event: 'AnyOther', count: '1', avg: '22.73', lds: '-', diff: '-', games: '4.40 '}
+ { name: 'Чемпионат африканских наций. 2018. Финальный турнир. Марокко. Групповой этап', date: '2018/01/22', event: 'AnyOther', count: '2', avg: '3.64', lds: '-', diff: '-', games: '26.00 29.00 '}
+, { name: 'Кубок АФК', date: '2018/01/22', event: 'AnyOther', count: '1', avg: '8.00', lds: '-', diff: '-', games: '12.50 '}
+, { name: 'Товарищеские матчи. Клубы', date: '2018/01/22', event: 'AnyOther', count: '2', avg: '18.60', lds: '-', diff: '-', games: '4.85 5.90 '}
+, { name: 'Англия. Премьер-лига', date: '2018/01/22', event: 'AnyOther', count: '1', avg: '25.32', lds: '16.32', diff: '-9.00', games: '3.95 '}
+, { name: 'Бразилия. Лига Кариока', date: '2018/01/22', event: 'AnyOther', count: '1', avg: '21.74', lds: '-', diff: '-', games: '4.60 '}
+, { name: 'Германия. 3-я лига', date: '2018/01/22', event: 'AnyOther', count: '1', avg: '8.70', lds: '12.62', diff: '3.92', games: '11.50 '}
+, { name: 'Египет. Премьер-лига', date: '2018/01/22', event: 'AnyOther', count: '2', avg: '8.00', lds: '7.06', diff: '-0.94', games: '14.50 10.50 '}
+, { name: 'Израиль. Премьер-лига', date: '2018/01/22', event: 'AnyOther', count: '1', avg: '14.29', lds: '5.30', diff: '-8.99', games: '7.00 '}
+, { name: 'Израиль. Лига Леумит', date: '2018/01/22', event: 'AnyOther', count: '3', avg: '7.37', lds: '8.72', diff: '1.35', games: '14.00 17.00 9.70 '}
+, { name: 'Индия. I-лига', date: '2018/01/22', event: 'AnyOther', count: '1', avg: '6.90', lds: '4.00', diff: '-2.90', games: '14.50 '}
+, { name: 'Индонезия. Кубок президента. Групповой этап. Тенггаронг', date: '2018/01/22', event: 'AnyOther', count: '2', avg: '16.46', lds: '-', diff: '-', games: '7.30 4.85 '}
+, { name: 'Испания. Примера дивизион', date: '2018/01/22', event: 'AnyOther', count: '1', avg: '9.52', lds: '12.63', diff: '3.11', games: '10.50 '}
+, { name: 'Италия. Серия А', date: '2018/01/22', event: 'AnyOther', count: '1', avg: '22.47', lds: '10.63', diff: '-11.84', games: '4.45 '}
+, { name: 'Италия. Серия C', date: '2018/01/22', event: 'AnyOther', count: '2', avg: '6.90', lds: '5.10', diff: '-1.80', games: '17.50 11.50 '}
+, { name: 'Кипр. 1-й дивизион', date: '2018/01/22', event: 'AnyOther', count: '1', avg: '15.15', lds: '16.55', diff: '1.40', games: '6.60 '}
+, { name: 'Мексика. Примера дивизион', date: '2018/01/22', event: 'AnyOther', count: '1', avg: '15.38', lds: '5.59', diff: '-9.79', games: '6.50 '}
+, { name: 'Нидерланды. 1-й дивизион', date: '2018/01/22', event: 'AnyOther', count: '2', avg: '17.62', lds: '15.38', diff: '-2.24', games: '4.75 6.60 '}
+, { name: 'ОАЭ. Арабиан Галф Лиг', date: '2018/01/22', event: 'AnyOther', count: '2', avg: '16.33', lds: '13.51', diff: '-2.82', games: '7.60 4.65 '}
+, { name: 'Турция. Суперлига', date: '2018/01/22', event: 'AnyOther', count: '1', avg: '11.11', lds: '11.80', diff: '0.69', games: '9.00 '}
+, { name: 'Турция. 1-я лига', date: '2018/01/22', event: 'AnyOther', count: '2', avg: '8.33', lds: '10.63', diff: '2.30', games: '11.00 13.00 '}
+, { name: 'Франция. Лига 2', date: '2018/01/22', event: 'AnyOther', count: '1', avg: '5.00', lds: '12.90', diff: '7.90', games: '20.00 '}
+, { name: 'Шотландия. Лига до 20 лет', date: '2018/01/22', event: 'AnyOther', count: '1', avg: '34.72', lds: '31.18', diff: '-3.54', games: '2.88 '}
+, { name: 'Женщины. Чемпионат КОНКАКАФ до 20 лет. 2018. Финальный турнир. Тринидад и Тобаго. Групповой этап', date: '2018/01/22', event: 'AnyOther', count: '1', avg: '77.52', lds: '-', diff: '-', games: '1.29 '}
+, { name: 'Женщины. Чемпионат мира. 2019. Отборочные матчи. Европа. 2-й групповой этап', date: '2018/01/22', event: 'AnyOther', count: '1', avg: '30.49', lds: '-', diff: '-', games: '3.28 '}
+, { name: 'Женщины. Чемпионат Южной Америки до 20 лет. 2018. Эквадор. Групповой этап', date: '2018/01/22', event: 'AnyOther', count: '1', avg: '27.47', lds: '-', diff: '-', games: '3.64 '}
+, { name: 'Женщины. Товарищеские матчи. Сборные', date: '2018/01/22', event: 'AnyOther', count: '2', avg: '15.75', lds: '-', diff: '-', games: '3.50 9.20 '}
 ];
