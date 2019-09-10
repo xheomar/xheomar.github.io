4e00068fd874ba83c094bbae7b74e06a5832310c
Heorhi Marazenka <yoga.yogich@gmail.com>
2019-09-10 13:42:27 +0300

    Adapt the code for automatic adding into TamperMonkey

--- VirtualBrestPictureEnabler.js
+++ VirtualBrestPictureEnabler.js
@@ -1,16 +1,19 @@
 // ==UserScript==
 // @name         VirtualBrestPictureEnabler
 // @namespace    http://tampermonkey.net/
-// @version      0.1
+// @version      0.2
 // @description  Find all blocked picture and enable access for them
 // @author       xheomar
 // @match        https://virtualbrest.by/*
 // @grant        none
 // ==/UserScript==
 
-$('p').find('img').each(function() {
-     var $img = $(this);
-     var imgSrcOld = $img.attr('src');
-     var imgSrcNew ='https://cdn.tribuna.com/fetch/?url=' + imgSrcOld;
-     $img.attr('src',imgSrcNew);
+(function() {
+    'use strict';
+    $('p').find('img').each(function() {
+        var $img = $(this);
+        var imgSrcOld = $img.attr('src');
+        var imgSrcNew ='https://cdn.tribuna.com/fetch/?url=' + imgSrcOld;
+        $img.attr('src',imgSrcNew);
 });
+})();
