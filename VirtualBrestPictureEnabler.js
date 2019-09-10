d3363e4e5c2a43a47ee26460d3ae93241d4a8023
Heorhi Marazenka <yoga.yogich@gmail.com>
2019-09-10 13:45:43 +0300

    Something wrong with adding into TamperMonkey

--- VirtualBrestPictureEnabler.js
+++ VirtualBrestPictureEnabler.js
@@ -1,9 +1,9 @@
 // ==UserScript==
 // @name         VirtualBrestPictureEnabler
 // @namespace    http://tampermonkey.net/
-// @version      0.2
-// @description  Find all blocked picture and enable access for them
-// @author       xheomar
+// @version      0.1
+// @description  Find all blocked pictures and enable access for them
+// @author       xheo
 // @match        https://virtualbrest.by/*
 // @grant        none
 // ==/UserScript==
@@ -15,5 +15,5 @@
         var imgSrcOld = $img.attr('src');
         var imgSrcNew ='https://cdn.tribuna.com/fetch/?url=' + imgSrcOld;
         $img.attr('src',imgSrcNew);
-});
+    })
 })();
